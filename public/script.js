60559cf75edc9150ecb684648aa51b8d4be594de
MuayBoy <zachderuiter@gmail.com>
2024-06-01 17:47:54 -0400

    added greater responsiveness for mobile

--- public/script.js
+++ public/script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function toggleTheme() {
+    const icons = document.querySelectorAll(".icon");
+    icons.forEach(function (icon) {
+        original_src = icon.src;
+        if (original_src.includes("light")) {
+            icon.src = original_src.replace("light", "dark");
+        } else if (original_src.includes("dark")) {
+            icon.src = original_src.replace("dark", "light");
+        }
+    });
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+}
